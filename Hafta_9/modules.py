397ae50adf7d684650cee31ca27c86c27f7d825a
furkanaydinn <furkan.aydin@bil.omu.edu.tr>
2026-04-06 17:12:12 +0300

    tarih modülü örnekleri

--- Hafta_9/modules.py
+++ Hafta_9/modules.py
@@ -1,83 +1,53 @@
 import datetime
-import math
 
+def simdiki_tarih():
+    return datetime.datetime.now()
 
+def tarih_bilesenleri():
+    x = datetime.datetime.now()
+    return {
+        "yil": x.year,
+        "ay": x.month,
+        "gun": x.day,
+        "saat": x.hour,
+        "dakika": x.minute
+    }
 
+def tarih_olustur(yil, ay, gun, saat=0, dakika=0, saniye=0):
+    return datetime.datetime(yil, ay, gun, saat, dakika, saniye)
 
+def gun_adi():
+    return datetime.datetime.now().strftime("%A")
 
+def ay_adi():
+    return datetime.datetime.now().strftime("%B")
 
-simdi = datetime.datetime.now()
-print(simdi)
+def formatli_tarih():
+    return datetime.datetime.now().strftime("%d/%m/%Y")
 
+def formatli_saat():
+    return datetime.datetime.now().strftime("%H:%M:%S")
 
-x = datetime.datetime.now()
+def haftanin_gunu_index():
+    return datetime.datetime.now().weekday()
 
-print("Yıl:", x.year)
-print("Ay:", x.month)
-print("Gün:", x.day)
-print("Saat:", x.hour)
-print("Dakika:", x.minute)
+def bugun_tarih():
+    return datetime.date.today()
 
-tarih = datetime.datetime(2025, 5, 17)
-print(tarih)
+def tarih_karsilastir(t1, t2):
+    return "t1 daha yeni" if t1 > t2 else "t2 daha yeni"
 
-tarih = datetime.datetime(2025, 5, 17, 14, 30, 0)
-print(tarih)
+def gun_farki(t1, t2):
+    return (t1 - t2).days
 
-x = datetime.datetime.now()
-print(x.strftime("%A"))
+def iso_format(tarih):
+    return tarih.isoformat()
 
-x = datetime.datetime.now()
-print(x.strftime("%B"))
+def tarih_to_string():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 
-x = datetime.datetime.now()
+def string_to_tarih(tarih_str):
+    return datetime.datetime.strptime(tarih_str, "%Y-%m-%d")
 
-print(x.strftime("%d/%m/%Y"))
-print(x.strftime("%H:%M:%S"))
-
-x = datetime.datetime.now()
-print(x.weekday())   # Pazartesi = 0
-
-bugun = datetime.date.today()
-print(bugun)
-
-
-t1 = datetime.datetime(2025, 1, 1)
-t2 = datetime.datetime(2024, 1, 1)
-
-if t1 > t2:
-    print("t1 daha yeni")
-else:
-    print("t2 daha yeni")
-
-
-
-
-t1 = datetime.datetime(2025, 1, 10)
-t2 = datetime.datetime(2025, 1, 1)
-
-fark = t1 - t2
-print("Gün farkı:", fark.days)
-
-
-d = datetime.date(2025, 1, 1)
-print(d.isoformat())
-
-
-x = datetime.datetime.now()
-
-metin = x.strftime("%Y-%m-%d %H:%M")
-print(metin)
-
-
-tarih_str = "2025-04-03"
-tarih = datetime.datetime.strptime(tarih_str, "%Y-%m-%d")
-
-print(tarih)
-
-
-dogum = datetime.datetime(2000, 1, 1)
-simdi = datetime.datetime.now()
-
-fark = simdi - dogum
-print("Yaş (gün):", fark.days)
+def yas_gun(dogum_tarihi):
+    return (datetime.datetime.now() - dogum_tarihi).days
---
+++ Hafta_9/new_module.py
@@ -0,0 +1,63 @@
+from modules import (
+    simdiki_tarih,
+    tarih_bilesenleri,
+    gun_adi,
+    ay_adi,
+    formatli_saat,
+    formatli_tarih,
+    iso_format,
+    haftanin_gunu_index,
+    tarih_karsilastir,
+    tarih_olustur,
+    bugun_tarih,
+    gun_farki,
+    tarih_to_string,
+    string_to_tarih,
+    yas_gun
+)
+
+import datetime
+
+def main():
+    print(" Şu anki tarih:", simdiki_tarih())
+    print(" Tarih bileşenleri:", tarih_bilesenleri())
+
+    print("\n Tarih oluşturma:")
+    print(tarih_olustur(2025, 5, 17))
+    print(tarih_olustur(2025, 5, 17, 14, 30))
+
+    print("\n Gün ve Ay:")
+    print("Gün:", gun_adi())
+    print("Ay:", ay_adi())
+
+    print("\n Formatlı çıktı:")
+    print(formatli_tarih())
+    print(formatli_saat())
+
+    print("\n Haftanın günü index:", haftanin_gunu_index())
+    print("Bugünün tarihi:", bugun_tarih())
+
+    print("\n Tarih karşılaştırma:")
+    t1 = datetime.datetime(2025, 1, 1)
+    t2 = datetime.datetime(2024, 1, 1)
+    print(tarih_karsilastir(t1, t2))
+
+    print("\n Gün farkı:")
+    print(gun_farki(
+        datetime.datetime(2025, 1, 10),
+        datetime.datetime(2025, 1, 1)
+    ))
+
+    print("\n ISO format:")
+    print(iso_format(datetime.date(2025, 1, 1)))
+
+    print("\n String dönüşümleri:")
+    print(tarih_to_string())
+    print(string_to_tarih("2025-04-03"))
+
+    print("\n Yaş (gün):")
+    print(yas_gun(datetime.datetime(2000, 1, 1)))
+
+
+if __name__ == "__main__":
+    main()
